a43785cb83791bbae43f2ebc256210020078e994
Tomas <tomas@augsignals.com>
2021-05-24 13:31:24 -0400

    Added output dialog for speckle filters and modified display name bugs

--- refined_lee_filter/refined_lee_filter.py
+++ refined_lee_filter/refined_lee_filter.py
@@ -30,6 +30,7 @@ from qgis.core import QgsMessageLog, Qgis, QgsProject, QgsRasterLayer, QgsMapLay
 from .resources import *
 # Import the code for the dialog
 from .refined_lee_filter_dialog import RefinedLeeFilterDialog
+from .output_dialog import OutputDialog
 import os.path
 
 
@@ -39,6 +40,7 @@ class RefinedLeeFilter:
     def __init__(self, iface):
         # Save reference to the QGIS interface
         self.iface = iface
+        self.output_dialog = OutputDialog()
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
         # initialize locale
@@ -229,13 +231,26 @@ class RefinedLeeFilter:
             exeName = "RefinedLee.exe"
             path = path + "/" + exeName
             args.insert(0, path)
+            args_message = " ".join(arg for arg in args)
 
-            QgsMessageLog.logMessage("Your plugin is executing with the following arguments")
-            QgsMessageLog.logMessage(str(args), 'AUG Plugins', Qgis.Info)
-            popen = subprocess.Popen(args)
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
             popen.wait()
+            out, err = popen.communicate()
+            output_dialog_text = ""
+            if out is not None:
+                output_dialog_text += out.decode('utf-8')
+            if err is not None:
+                output_dialog_text += err.decode('utf-8')
+        
             QgsMessageLog.logMessage("Your plugin code has been executed correctly", 'MyPlugin', Qgis.Info)
+            QgsMessageLog.logMessage(str(args), 'MyPlugin', Qgis.Info)
+            print("output is", out, err)
+            QgsMessageLog.logMessage(str(out), 'MyPlugin', Qgis.Info)
+            QgsMessageLog.logMessage(str(err), 'MyPlugin', Qgis.Info)
+            self.output_dialog.commandText.setText(args_message)
+            self.output_dialog.outputText.setText(output_dialog_text)
+            test = self.output_dialog.exec_()
             output_path = self.dlg.outputQgsFileWidget.filePath()
             rlayer = QgsRasterLayer(output_path, os.path.basename(output_path))
             if not rlayer.isValid():
-                print("Layer failed to load!")
+                QgsMessageLog.logMessage("Layer failed to load!", 'MyPlugin', Qgis.Info)
